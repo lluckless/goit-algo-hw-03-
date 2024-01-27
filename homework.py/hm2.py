ca33a47833c90dfeee52fdf20a17020ca870f8fd
lluckless <ahmmatt@gmail.com>
2024-01-27 10:20:14 +0200

    add comment

--- homework.py/hm2.py
+++ homework.py/hm2.py
@@ -1,27 +1,29 @@
 import random
 
-def get_number_ticket(min,max,quantity):
-        if 1 <= min < max <= 1000 and 1 <= quantity <= (max-min+1):
-            numbers = range(min, max+1)
-            happyTicket = random.sample(numbers, quantity)
-            happyTicket.sort()
-            return happyTicket
+def get_number_ticket(min, max, quantity):
+    
+    if 1 <= min < max <= 1000 and 1 <= quantity <= (max-min+1):   
+        numbers = range(min, max+1)                               # Створення списку [1-1000]
+        happyTicket = sorted(random.sample(numbers, quantity))    # Створення відсортованної вибірки
+        return happyTicket
         
-        else:
-            print("\nValues can be [1-1000]")
-            print("All ploho давай по новой")
-            return []
+    else:
+        print("\nValues can be [1-1000]")
+        print("All ploho давай по новой")
+        return []
 
 while True: 
-        try:
-            min = int(input("Enter a min int number: "))
-            max = int(input("Enter a max int number: "))
-            quantity = int(input("Enter int quantity: "))
-        except Exception:
-             continue
-        answer = get_number_ticket(min, max, quantity)
-        if answer == []:
-            continue
-        else:
-            print(answer)
-            break
+    try:                                                      # Перевірка вводу корректних данних
+        min = int(input("Enter a min int number: "))          
+        max = int(input("Enter a max int number: "))          
+        quantity = int(input("Enter int quantity: "))
+    
+    except Exception:
+        continue
+    
+    answer = get_number_ticket(min, max, quantity)            # Виклик функції
+    if answer == []:
+        continue
+    else:
+        print(answer)
+        break
