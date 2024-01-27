f3a3d6ba5cebaae994c5ff25c143e67c509f46bb
lluckless <ahmmatt@gmail.com>
2024-01-27 10:03:18 +0200

    add comment

--- homework.py/hm1.py
+++ homework.py/hm1.py
@@ -3,8 +3,8 @@ from datetime import datetime as dtdt
 
 def get_days_from_today():
     try:
-        datetime_object = dtdt.strptime(input("Input date: "), "%d-%m-%Y").date()
-        current_date = dtdt.today().date()
+        datetime_object = dtdt.strptime(input("Input date in format DD-MM-YYYY: "), "%d-%m-%Y").date() # Об'явлення необхідної дати(без часу) та обробка її формату
+        current_date = dtdt.today().date()                                        # Об'явлення сьогоднішньої дати(без часу)
         diff_date = current_date.toordinal() - datetime_object.toordinal()
         print(diff_date)
            
