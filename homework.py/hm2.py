5e95c39bd7e979cddb76e4dcc370bcbeff793bc4
lluckless <ahmmatt@gmail.com>
2024-01-28 12:34:52 +0200

    update comment

--- homework.py/hm2.py
+++ homework.py/hm2.py
@@ -2,7 +2,7 @@ import random
 
 def get_number_ticket(min, max, quantity):
     
-    if 1 <= min < max <= 1000 and 1 <= quantity <= (max-min+1):   
+    if 1 <= min < max <= 1000 and 1 <= quantity <= (max-min+1):   # Перевірка вводу корректних данних
         numbers = range(min, max+1)                               # Створення списку [1-1000]
         happyTicket = sorted(random.sample(numbers, quantity))    # Створення відсортованної вибірки
         return happyTicket
@@ -13,7 +13,7 @@ def get_number_ticket(min, max, quantity):
         return []
 
 while True: 
-    try:                                                      # Перевірка вводу корректних данних
+    try:                                                      # Блок перевірки з ймовірністю помилки
         min = int(input("Enter a min int number: "))          
         max = int(input("Enter a max int number: "))          
         quantity = int(input("Enter int quantity: "))
